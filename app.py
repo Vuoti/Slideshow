9060aa7cef49cb0d44810ae61162448035fcaf8b
Andre <andre@ditached.de>
2025-12-20 13:12:43 +0100

    port

--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def upload_image():
         return jsonify({"success": True, "filename": unique_filename}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=6001)
